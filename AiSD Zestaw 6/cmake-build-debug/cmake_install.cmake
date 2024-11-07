02a920c15403d274054938bfd85669a5c231792c
Agnieszka Kowalczuk <agamaria05@gmail.com>
2024-11-07 13:56:57 +0100

    Remove obsolete cache file from CMake build directory

--- AiSD Zestaw 6/cmake-build-debug/cmake_install.cmake
+++ AiSD Zestaw 6/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/agama/CLionProjects/AiSD/AiSD 6
+# Install script for directory: C:/Users/agama/CLionProjects/AiSD/AiSD Zestaw 6
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/agama/CLionProjects/AiSD/AiSD 6/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/agama/CLionProjects/AiSD/AiSD Zestaw 6/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
